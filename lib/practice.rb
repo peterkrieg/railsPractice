89e7bbc7b93071cd6a07ee094e24a0bdccc4239e
Peter Krieg <pkrieg2@gmail.com>
2015-04-06 02:19:27 -0400

    added rest of rails automatic stuff to track

---
+++ lib/practice.rb
@@ -0,0 +1,30 @@
+def stupidPrint
+	puts 'what what'  \
+	' what what'
+end
+
+# stupidPrint
+
+def boolPrac
+	bool = false
+
+	if !bool
+		puts 'bool is true'
+	else 
+		puts 'bool is false'
+	end
+end
+
+# boolPrac
+
+
+
+def loopPractice
+	counter = 1
+	while counter <= 10
+		puts counter
+		counter += 1
+	end
+end
+
+loopPractice
