925ba3b2fd2ab0b87c63f6653441e4100526b216
Peter Krieg <pkrieg2@gmail.com>
2015-04-06 17:24:36 -0400

    added some code for contact page controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   root to: 'visitors#new'
 
+  post 'contact', to: 'contacts#process_form'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
